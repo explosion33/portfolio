1a77409c3e6f6a26466cff2b3d2e64c983dfbe67
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2023-01-03 21:45:04 -0800

    added new blog page

--- src/routes.rs
+++ src/routes.rs
@@ -19,6 +19,21 @@ fn project(project: String) -> Template {
     Template::render(project, rocket_dyn_templates::context!{})
 }
 
+#[rocket::get("/blog/<blog>")]
+fn blog(blog: String) -> Template {
+    Template::render(blog, rocket_dyn_templates::context!{})
+}
+
+#[rocket::get("/blog/static/<file>")]
+async fn get_file_blog(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("public/").join(file)).await.ok()
+}
+
+#[rocket::get("/blog/img/<blg>/<file>", rank=1)]
+async fn get_image_blog(blg: PathBuf, file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("blog_images/").join(Path::new(&blg)).join(&file)).await.ok()
+}
+
 #[rocket::get("/static/<file>")]
 async fn get_file(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("public/").join(file)).await.ok()
@@ -40,7 +55,7 @@ pub fn start_api() {
         .expect("create tokio runtime")
         .block_on(async move {
             let _ = rocket::build()
-            .mount("/", rocket::routes![index, get_file, project, get_project_image])
+            .mount("/", rocket::routes![index, get_file, project, get_project_image, blog, get_file_blog, get_image_blog])
             .attach(Template::fairing())
             //.manage()
             .launch()
